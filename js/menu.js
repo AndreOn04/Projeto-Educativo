62f7e0d2dfb55e3a17e72111749e8b95518bd00b
Marcio Andre <andreonlinemt@yahoo.com>
2026-01-19 16:10:15 -0400

    Commit inicial do projeto - Violencia Domestica

---
+++ js/menu.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navMenu = document.querySelector('.menu');
+    const menuIcon = document.getElementById('menu-icon');
+
+    menuToggle.addEventListener('click', () => {
+        // Abre ou fecha o menu
+        navMenu.classList.toggle('is-active');
+        
+        // Verifica se o menu está aberto para trocar o ícone
+        if (navMenu.classList.contains('is-active')) {
+            menuIcon.setAttribute('name', 'close-outline'); // Troca para o X
+        } else {
+            menuIcon.setAttribute('name', 'menu-outline');  // Volta para o Hambúrguer
+        }
+    });
+});
