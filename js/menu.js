0441f39b68c71f0c449d07fed7fe916074b2d7eb
Marcio Andre <andreonlinemt@yahoo.com>
2026-01-21 15:59:47 -0400

    Novas implementações, navegações, botões, responsividade

--- js/menu.js
+++ js/menu.js
@@ -1,17 +1,52 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+    function closeAllSubmenus() {
+        document.querySelectorAll('.submenu').forEach(sub => {
+            sub.classList.remove('is-active-desktop');
+        });
+        document.querySelectorAll('.dropdown-arrow').forEach(arrow => {
+            arrow.classList.remove('is-open');
+        });
+    }
+
     const menuToggle = document.querySelector('.menu-toggle');
     const navMenu = document.querySelector('.menu');
     const menuIcon = document.getElementById('menu-icon');
 
-    menuToggle.addEventListener('click', () => {
-        // Abre ou fecha o menu
-        navMenu.classList.toggle('is-active');
-        
-        // Verifica se o menu está aberto para trocar o ícone
-        if (navMenu.classList.contains('is-active')) {
-            menuIcon.setAttribute('name', 'close-outline'); // Troca para o X
-        } else {
-            menuIcon.setAttribute('name', 'menu-outline');  // Volta para o icon menu
+    if (menuToggle) {
+        menuToggle.addEventListener('click', () => {
+            navMenu.classList.toggle('is-active');
+            if(navMenu.classList.contains('is-active')) {
+                menuIcon.setAttribute('name', 'close-outline');
+            } else {
+                menuIcon.setAttribute('name', 'menu-outline');
+                closeAllSubmenus();
+            }
+        });
+    }
+
+    document.querySelectorAll('.dropdown-toogle').forEach(toggle => {
+        toggle.addEventListener('click', (e) => {
+            e.preventDefault();
+            
+            const targetId = toggle.getAttribute('data-target');
+            const targetSubmenu = document.getElementById(targetId);
+            const targetArrow = toggle.querySelector('.dropdown-arrow');
+
+            const isOpen = targetSubmenu.classList.contains('is-active-desktop');
+
+            closeAllSubmenus();
+
+            if (!isOpen) {
+                targetSubmenu.classList.add('is-active-desktop');
+                if (targetArrow) targetArrow.classList.add('is-open');
+            }
+        });
+    });
+
+    document.addEventListener('click', (e) => {
+        if(!e.target.closest('.menu-item-dropdown') && !e.target.closest('.menu-toggle')) {
+            closeAllSubmenus();
         }
     });
 });
